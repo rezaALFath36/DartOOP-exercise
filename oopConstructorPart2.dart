3f3c80db32089a540b7b1dbb183aed4fee0d504c
RezaOuzaa_ <rezarezaalfath@gmail.com>
2026-06-07 18:30:56 +0700

    Create oopConstructorPart2.dart

---
+++ oopConstructorPart2.dart
@@ -0,0 +1,19 @@
+class Armor{
+  String nama;
+  int defense;
+  String tipe;
+  
+  Armor({required this.nama, required this.defense, this.tipe = 'Besi'});
+  
+void info(){
+    print('Armor $nama [Tipe: $tipe] dipasang! (+ $defense Defense)');
+  }
+}
+
+void main(){
+  Armor armor1 = Armor(nama : 'Rompi',defense: 50);
+  Armor armor2 = Armor(nama : 'Jubah Gaib',defense: 30, tipe: 'kain');
+  
+  armor1.info();
+  armor2.info();
+}
