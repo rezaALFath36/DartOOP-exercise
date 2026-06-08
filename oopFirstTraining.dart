27dfed1b2820d1d149aaf52e8b5463b226d70518
RezaOuzaa_ <rezarezaalfath@gmail.com>
2026-06-08 21:07:13 +0700

    Update oopFirstTraining.dart

--- oopFirstTraining.dart
+++ oopFirstTraining.dart
@@ -1,18 +1,24 @@
+//Class, Object and Method
+//Class
 class Hero {
   String nama = '';
   int darah = 0;
-  
+
+//Method
   void kenaSerang(){
     darah -= 20;
     print('$nama meringis : aduh, darahku berkurang!');
   }
 }
 
+//Object
 void main(){
   Hero hero1 = Hero();
   hero1.nama = 'Kael';
   hero1.darah = 100;
-  hero1.kenaSerang();
+  
+//Calling Method
+  hero1.kenaSerang();
   
   print('Nama : ${hero1.nama} dan memiliki nyawa ${hero1.darah}');
 }
