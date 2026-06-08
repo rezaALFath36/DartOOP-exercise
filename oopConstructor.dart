0a979b4a8e06f10ab05b20eca754e54e8210e1f8
RezaOuzaa_ <rezarezaalfath@gmail.com>
2026-06-08 21:08:25 +0700

    Update oopConstructor.dart

--- oopConstructor.dart
+++ oopConstructor.dart
@@ -1,7 +1,9 @@
+//Constructor Part 1
 class Senjata{
   String nama = '';
   int damage = 0;
-  
+
+//Constructor
   Senjata(String namaBaru, int damageBaru){
     nama = namaBaru;
     damage = damageBaru;
